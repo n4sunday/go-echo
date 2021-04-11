3b9ac4457affe51ee38b23f73827cef67044828a
Sunday <nsoooooooooosn@gmail.com>
2021-04-11 14:11:12 +0700

    add env

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-echo
 
 go 1.16
 
-require github.com/labstack/echo/v4 v4.2.2 // indirect
+require (
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/labstack/echo/v4 v4.2.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	var errEnv error
+	errEnv = godotenv.Load()
+	if errEnv != nil {
+		log.Fatalf("Error getting env, %v", errEnv)
+	}
+
 	e := echo.New()
+	port := fmt.Sprintf(":%v", os.Getenv("PORT"))
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+
+	e.Logger.Fatal(e.Start(port))
 }
